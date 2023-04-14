685135eb31675319dbc7736ddafd25daf5112b59
Michal Koutný <mkoutny@suse.com>
2023-04-14 19:36:45 +0200

    job: Implement restart jobs for rtemplate handle services

--- src/core/unit.c
+++ src/core/unit.c
@@ -2721,7 +2721,12 @@ static bool unit_process_job(Job *j, UnitActiveState ns, bool reload_success) {
 
                 if (UNIT_IS_INACTIVE_OR_FAILED(ns))
                         job_finish_and_invalidate(j, JOB_DONE, true, false);
-                else if (j->state == JOB_RUNNING && ns != UNIT_DEACTIVATING) {
+                else if (j->type == JOB_RESTART && j->unit_next && UNIT_IS_ACTIVE_OR_RELOADING(ns)) {
+                        /* We expect SERVICE_RUNNING_PASSIVE */
+                        if (ns == UNIT_ACTIVE)
+                                job_finish_and_invalidate(j, JOB_DONE, true, false);
+                        /* SERVICE_PASSIVATE job runs... */
+                } else if (j->state == JOB_RUNNING && ns != UNIT_DEACTIVATING) {
                         unexpected = true;
                         job_finish_and_invalidate(j, JOB_FAILED, true, false);
                 }
